e55769bc04aa4d2e7b01371728ca493ff5846a85
zhaoxin <zhaoxin@tofflon.com>
2021-10-28 13:58:48 +0800

    part04-2

---
+++ service-core/src/main/java/com/atguigu/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -0,0 +1,42 @@
+package com.atguigu.srb.core.controller.admin;
+
+import com.atguigu.srb.core.pojo.entity.IntegralGrade;
+import com.atguigu.srb.core.service.IntegralGradeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author: admin
+ * @date: 2021/10/28 11:09
+ * @description:
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/admin/core/integralGrade")
+@Api(tags = "积分等级管理")
+public class AdminIntegralGradeController {
+
+    @Resource
+    private IntegralGradeService integralGradeService;
+
+    @ApiOperation("积分等级列表")
+    @GetMapping("/list")
+    public List<IntegralGrade> listAll(){
+        return integralGradeService.list();
+    }
+
+
+    @ApiOperation(value = "根据id删除积分等级", notes = "逻辑删除")
+    @DeleteMapping("/remove/{id}")
+    public boolean removeById(
+            @ApiParam(value = "数据id", required = true, example = "100")
+            @PathVariable Long id){
+        return integralGradeService.removeById(id);
+    }
+
+}
