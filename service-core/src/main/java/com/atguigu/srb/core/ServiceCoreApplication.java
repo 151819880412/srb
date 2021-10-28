e949a0af19a121ddca65626bb0bd654850334b22
zhaoxin <zhaoxin@tofflon.com>
2021-10-28 10:55:18 +0800

    为知 part04-01完成，正常启动8110

---
+++ service-core/src/main/java/com/atguigu/srb/core/ServiceCoreApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.srb.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author: admin
+ * @date: 2021/10/28 10:49
+ * @description:
+ */
+@SpringBootApplication
+@ComponentScan({"com.atguigu.srb"})
+public class ServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+}
---
+++ service-core/src/main/java/com/atguigu/srb/core/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.atguigu.srb.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author: admin
+ * @date: 2021/10/28 10:48
+ * @description:
+ */
+@Configuration
+@MapperScan("com.atguigu.srb.core.mapper")
+@EnableTransactionManagement //事务处理
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//分页
+        return interceptor;
+    }
+}
