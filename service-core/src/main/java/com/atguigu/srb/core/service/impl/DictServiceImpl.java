d6026e2780b67b0397224509c6893caa1de84530
zhaoxin <zhaoxin@tofflon.com>
2021-10-29 09:26:50 +0800

    part07-3

--- service-core/src/main/java/com/atguigu/srb/core/service/impl/DictServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/DictServiceImpl.java
@@ -8,10 +8,13 @@ import com.atguigu.srb.core.mapper.DictMapper;
 import com.atguigu.srb.core.service.DictService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -25,10 +28,30 @@ import java.io.InputStream;
 @Slf4j
 public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
 
+    /**
+     * @Resource
+     * private DictMapper dictMapper;
+     * 如果要注入的 Mapper 就是当前 service 下的 Mapper name就可以不用写 @Resource 直接写 baseMapper
+     */
+
     @Transactional(rollbackFor = Exception.class)   // 事务
     @Override
     public void importData(InputStream inputStream) {
         EasyExcel.read(inputStream, ExcelDictDTO.class,new ExcelDictDTOListener(baseMapper)).sheet().doRead();
         log.info("Excel 导入成功");
+    }
+
+    @Override
+    public List<ExcelDictDTO> listDictData() {
+
+        List<Dict> dictList = baseMapper.selectList(null);
+        // 创建 ExcelDictDTO 列表，将 Dic 列表转换成 ExcelDictDTO 列表
+        ArrayList<ExcelDictDTO> excelDictDTOList = new ArrayList<>(dictList.size());
+        dictList.forEach(dict ->{
+            ExcelDictDTO excelDictDTO = new ExcelDictDTO();
+            BeanUtils.copyProperties(dict,excelDictDTO);
+            excelDictDTOList.add(excelDictDTO);
+        });
+        return excelDictDTOList;
     }
 }
