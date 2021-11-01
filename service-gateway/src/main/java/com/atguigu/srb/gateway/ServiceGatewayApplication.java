724d28d02018b3c70138f2d7b11d842d2328f73d
zhaoxin <zhaoxin@tofflon.com>
2021-11-01 11:11:21 +0800

    part17-1

---
+++ service-gateway/src/main/java/com/atguigu/srb/gateway/ServiceGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.srb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: admin
+ * @date: 2021/10/29 16:02
+ * @description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+}
