2af83a514807e250198c4249f80fd23138889504
zhaoxin <zhaoxin@tofflon.com>
2021-11-16 14:41:32 +0800

    part21-3

--- service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowInfoServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowInfoServiceImpl.java
@@ -2,6 +2,10 @@ package com.atguigu.srb.core.service.impl;
 
 import com.atguigu.common.exception.Assert;
 import com.atguigu.common.result.ResponseEnum;
+import com.atguigu.srb.core.enums.BorrowAuthEnum;
+import com.atguigu.srb.core.enums.BorrowInfoStatusEnum;
+import com.atguigu.srb.core.enums.BorrowerStatusEnum;
+import com.atguigu.srb.core.enums.UserBindEnum;
 import com.atguigu.srb.core.mapper.IntegralGradeMapper;
 import com.atguigu.srb.core.mapper.UserInfoMapper;
 import com.atguigu.srb.core.pojo.entity.BorrowInfo;
@@ -52,5 +56,40 @@ public class BorrowInfoServiceImpl extends ServiceImpl<BorrowInfoMapper, BorrowI
         }
 
         return integralGrade.getBorrowAmount();
+    }
+
+    @Override
+    public void saveBorrowInfo(BorrowInfo borrowInfo, Long userId) {
+
+        // 获取userInfo信息
+        UserInfo userInfo = userInfoMapper.selectById(userId);
+
+        // 判断用户绑定状态
+        Assert.isTrue(
+                userInfo.getBindStatus().intValue() == UserBindEnum.BIND_OK.getStatus().intValue(),
+                ResponseEnum.USER_NO_BIND_ERROR
+        );
+
+        // 判断借款人额度申请状态
+        Assert.isTrue(
+                userInfo.getBorrowAuthStatus().intValue() == BorrowAuthEnum.AUTH_OK.getStatus().intValue(),
+                ResponseEnum.USER_NO_AMOUNT_ERROR
+        );
+
+        // 判断借款人额度是否充足
+        BigDecimal borrowAmount = this.getBorrowAmount(userId);
+        Assert.isTrue(
+                borrowInfo.getAmount().doubleValue() <= borrowAmount.doubleValue(),
+                ResponseEnum.USER_AMOUNT_LESS_ERROR
+        );
+
+        // 储存borrowInfo数据
+        // 百分比转小数
+        borrowInfo.setUserId(userId);
+        borrowInfo.setBorrowYearRate(borrowInfo.getBorrowYearRate().divide(new BigDecimal(100)));
+        // 设置借款申请的审核状态
+        borrowInfo.setStatus(BorrowInfoStatusEnum.CHECK_RUN.getStatus());
+        baseMapper.insert(borrowInfo);
+
     }
 }
